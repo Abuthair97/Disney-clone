8d751eb398d00ce54c0875b54ad65feff60d3e42
Sheika Abuthair <105582784+Abuthair97@users.noreply.github.com>
2022-09-27 21:21:41 +0530

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,6 +1,9 @@
 let movies = [
     {
       name: "falcon and the winter soldier",
+        
+        
+        
       des:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
       image: "images/slider 2.png"
